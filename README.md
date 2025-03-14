854d85056a3fdafe26be07e6a21f87ac481fed7d
rijav1001 <rvu100199@gmail.com>
2025-03-14 16:04:26 +0530

    TDD: Added final test cases for different scenarios and a readme file

---
+++ README.md
@@ -0,0 +1,14 @@
+# TDD Kata 1
+
+This is a Test Driven Development assessment where a simple string calculator has been written in Python, and being test for different scenarios using pytest library.
+
+# Requirements
+
+The only requirement to run the test cases is to install pytest
+Command: pip install pytest or pip3 install pytest, and then pytest -v
+
+# Screenshots 
+
+Screenshots have been added showing the result of the test cases, to be found under the "screenshots" directory
+
+THANK YOU...!!!
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -16,3 +16,20 @@ def test_ignore_larger_numbers():
 def test_negative_numbers_not_allowed():
     with pytest.raises(ValueError, match="negative numbers not allowed: -2,-3"):
         add("1,-2,3,-3")
+
+def test_single_number():
+    assert add("1") == 1
+    assert add("5") == 5
+
+def test_two_numbers():
+    assert add("1,2") == 3
+
+def test_multiple_numbers():
+    assert add("1,2,3") == 6
+    assert add("4,5,6,7") == 22
+
+def test_multiple_delimiters():
+    assert add("//[*][%]\n1*2%3") == 6
+
+def test_multiple_delimiters_with_different_lengths():
+    assert add("//[**][%%]\n1**2%%3") == 6
