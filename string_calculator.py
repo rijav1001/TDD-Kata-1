c0400a177f1c4c6c44d38e2c22ae21a56fc61556
rijav1001 <rvu100199@gmail.com>
2025-03-14 15:45:03 +0530

    TDD: Added test cases for checking newline and custom delimeters

--- string_calculator.py
+++ string_calculator.py
@@ -4,3 +4,15 @@ def add(nums: str) -> int:
     if not nums:
         return 0
     
+    # check the string for delimeters, extract regex pattern for those delimeters and then split the nums such that our resultant output is just the numbers to be added
+    if nums.startswith("//"):
+        delimiters, nums = nums[2:].split("\n", 1)
+        delimiters = re.findall(r"\[([^\]]+)\]", delimiters) or [delimiters]
+        pattern = "|".join(map(re.escape, delimiters))
+        nums = re.split(pattern, nums)
+    else:
+        nums = re.split("[,\n]", nums)
+
+    nums_list = [int(n) for n in nums if int(n) <= 1000]
+
+    return sum(nums_list)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -3,3 +3,9 @@ from string_calculator import add
 
 def test_empty_string_returns_zero():
     assert add("") == 0
+
+def test_newline_delimiter():
+    assert add("1\n2,3") == 6
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
